d6d826b511fd9d3e249bc80ea556781883b793e2
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-07-22 10:27:25 +0300

    Add notifications feature: update app layout, add NotificationsList component, interfaces, and store
    - Updated  and  for new dependencies.
    - Modified  to include notifications feature.
    - Added  component for displaying notifications.
    - Added  for notification interface definition.
    - Added  for managing notification state.
    - Added  for defining notification types.
    - Updated  to integrate with the new notifications feature.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavBar from './components/common/NavBar.vue';
+import Notifications from '@/components/common/NotificationsList.vue'
+
 
 </script>
 
 <template>
   <NavBar/>
   <RouterView />
+  <Notifications />
 </template>
 
 <style scoped>
