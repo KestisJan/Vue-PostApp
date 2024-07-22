42fcd6ea939c38df9f7124be8e923e04c2b8058f
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-07-22 09:09:17 +0300

    feat: Add 404 Not Found page and update router configuration
    - Modified router configuration in src/router/index.ts to handle 404 routes
    - Added new NotFoundView.vue for 404 Not Found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AuthorsView from '@/views/AuthorsView.vue'
-
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +11,11 @@ const router = createRouter({
       alias: '/authors',
       component: AuthorsView
     },
+    {
+      path: '/:catchAll(.*)',
+      name: 'notfound',
+      component: NotFoundView
+    }
   ]
 })
 
