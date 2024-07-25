f0a6ee2ab6529c650ea84779f22a7b0cf46ad338
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-07-25 15:56:05 +0300

    Implement and enhance  method in AuthorStore
    - Added  method to handle author updates with validation for , , and uid=1000(kestas) gid=1000(kestas) groups=1000(kestas),4(adm),24(cdrom),27(sudo),30(dip),46(plugdev),100(users),114(lpadmin).
    - Integrated  for PATCH requests to  endpoint.
    - Added default parameters for pagination in the  method call post-update.
    - Improved error handling with appropriate notifications for success and failure cases.

--- src/stores/AuthorStore.ts
+++ src/stores/AuthorStore.ts
@@ -45,10 +45,34 @@ export const useAuthorsStore = defineStore('authors-store', () => {
         }
 
         try {
-            const response = await axiosService.postData('/aythors', author);
+            const response = await axiosService.postData('/authors', author);
             notificationStore.success('Author added successfully!')
         } catch (err: any) {
             console.error('Failed to add author:', err)
+            notificationStore.warning('Error fetching data:' + err)
+        }
+    }
+
+    const updateAuthor = async (author: IAuthor) => {
+        try {
+            if (!author) throw Error('Failed to update author, no author provided')
+            if (!author.name) throw Error('Failed to update author, author must have a name')
+            if (!author.surname) throw Error('Failed to update author, author must have a name')
+            if (!author.id) throw Error('Failed to update author, author must have an id')
+
+            const response = await axiosService.updateData('/authors', author.id, author)
+
+            notificationStore.success(`Updated author ${author.id} ${author.name} ${author.surname}`)
+
+            const defaultPage = 1;
+            const defaultLimit = 5;
+            const defaultQuery = '';
+
+            await fetchAuthors(defaultPage, defaultLimit, defaultQuery)
+            return response;
+        } catch (err: any) {
+            console.error('Failed to add author:', err)
+            notificationStore.warning('Error: ' + err.message)
         }
     }
 
@@ -56,6 +80,7 @@ export const useAuthorsStore = defineStore('authors-store', () => {
     return {
         authors: readonly(authors),
         fetchAuthors,
-        addAuthor
+        addAuthor,
+        updateAuthor
     }
 });
